33a4abfe78f8a3c74ce4733b767e8496087d45c1
hectorstudio <david.liu987@hotmail.com>
2020-01-29 06:09:50 -0300

    Update index.js

--- src/screens/AddTask/index.js
+++ src/screens/AddTask/index.js
@@ -5,6 +5,8 @@ import {
   Modal,
   StyleSheet,
   TouchableWithoutFeedback,
+  TouchableOpacity,
+  TextInput,
 } from 'react-native';
 
 import commonStyles from '../../commonStyles';
@@ -20,6 +22,23 @@ export default class AddTask extends Component {
         <TouchableWithoutFeedback onPress={this.props.onCancel}>
           <View style={styles.background} />
         </TouchableWithoutFeedback>
+
+        <View style={styles.container}>
+          <Text style={styles.header}>New tasks</Text>
+          <TextInput style={styles.input} />
+          <View style={styles.buttons}>
+            <TouchableOpacity>
+              <Text style={styles.textButton}>Cancel</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.textButton}>Save</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <TouchableWithoutFeedback onPress={this.props.onCancel}>
+          <View style={styles.background} />
+        </TouchableWithoutFeedback>
       </Modal>
     );
   }
@@ -29,5 +48,34 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  container: {
+    backgroundColor: '#fff',
+  },
+  header: {
+    fontFamily: commonStyles.fontFamily,
+    backgroundColor: commonStyles.colors.today,
+    color: commonStyles.colors.secondary,
+    textAlign: 'center',
+    padding: 5,
+    fontSize: 15,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  textButton: {
+    margin: 20,
+    marginRight: 30,
+    color: commonStyles.colors.today,
+  },
+  input: {
+    fontFamily: commonStyles.fontFamily,
+    height: 40,
+    margin: 10,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 6,
   },
 });
